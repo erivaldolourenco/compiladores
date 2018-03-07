51f303126f6c51b56fa362bda2b64b24b4d865dc
erivaldolourenco <erivaldolourenco.m@gmail.com>
2018-03-07 10:19:45 -0300

    Atualizando

--- lexical/lexicalanalyzer.py
+++ lexical/lexicalanalyzer.py
@@ -6,62 +6,69 @@ class Lexeme(object):
         self.file = file
 
     def analizer(self):
-    	li = 0
-    	col = 0
-    	list_of_lexema = []
+        li = 0
+        col = 0
+        list_of_lexema = []
         with open(self.file) as file:
             filelines = file.readlines()
             # print(filelines)
             for line in filelines:
-            	list_of_lexema = []
-            	li += 1
-            	lexema = ''
-            	# print("LINHA"+str(li)+": "+ line)
-            	# list_of_lexema.append(lexema)
+                list_of_lexema = []
+                li += 1
+                lexema = ''
+                # print("LINHA"+str(li)+": "+ line)
+                # list_of_lexema.append(lexema)
 
-            	for caracter in list(line):
-            		# print("--"+caracter+"--")
-            		if re.findall('[^A-Za-z0-9]',caracter):
-            			# print("CARACTER ESPECIAL")
-            			if caracter == ' ' and lexema.__len__() == 0:
-            				col += 1
-            				lexema == ''
+                for caracter in list(line):
+                    # print("--"+caracter+"--")
+                    if re.findall('[^A-Za-z0-9]',caracter):
+                        # print("CARACTER ESPECIAL")
+                        if caracter == ' ':
+                            print("ESPACO/ LEXEMA ->"+str(lexema.__len__()))
+                            if lexema.__len__() == 0:
+                                col += 1
+                                lexema == ''
+                            else:
+                                list_of_lexema.append(lexema)
+                                print(lexema)
+                                col += 1
+                                lexema == ''
 
-	            		elif caracter == '(':
-	            			list_of_lexema.append(lexema)
-	            			
-	            			list_of_lexema.append(caracter)
-            				col += 1
-            				lexema = ''
-	            		elif caracter == ')':
-	            			lexema += caracter
-	            			list_of_lexema.append(lexema)
-            				col += 1
-            				lexema = ''
-	            		elif caracter == '\n':
-	            			list_of_lexema.append(lexema)
-	            			col += 1
-	            			lexema = ''
-	            		elif caracter == '\t':
-	            			col += 1
-	            		else:
-	            			lexema += caracter
-	            			list_of_lexema.append(lexema)
-	            			lexema = ''
-            				col += 1
-	            	else:
-	            		# print("ALFA NUMERICO")
-            			lexema += caracter
-            			col += 1
+                        elif caracter == '(':
+                            list_of_lexema.append(lexema)
+                            print(lexema)
+                            list_of_lexema.append(caracter)
+                            col += 1
+                            lexema = ''
+                        elif caracter == ')':
+                            lexema += caracter
+                            list_of_lexema.append(lexema)
+                            col += 1
+                            lexema = ''
+                        elif caracter == '\n':
+                            list_of_lexema.append(lexema)
+                            col += 1
+                            lexema = ''
+                        elif caracter == '\t':
+                            col += 1
+                        else:
+                            lexema += caracter
+                            list_of_lexema.append(lexema)
+                            lexema = ''
+                            col += 1
+                    else:
+                        # print("ALFA NUMERICO")
+                        lexema += caracter
+                        col += 1
 
 
-           	print(list_of_lexema)
-           	
+            print(list_of_lexema)
+            
 
 
 
-                	# print("->" + caracter)
-                	# col += 1
+                    # print("->" + caracter)
+                    # col += 1
 
         # print("Linha:"+str(li))
         # print("Coluna"+str(col))
